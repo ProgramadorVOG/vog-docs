3606459c3892683a80e4527da32ac8a4c4661e81
Alexander Rodriguez <xalecr30@gmail.com>
2025-10-03 16:07:50 -0400

    proyecto inicial

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Virtual Office Group',
+			defaultLocale: 'es',
+			credits: false,
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ProgramadorVOG' }],
+			sidebar: [
+				{
+					label: 'AL',
+					collapsed: false,
+					items:[
+						{
+							label: 'Guia de desarrollo',
+							collapsed: false,
+							autogenerate: { directory: 'al/guides' }
+						},
+						{
+							label: 'Publicar en Microsoft AppSource',
+							collapsed: false,
+							autogenerate: { directory: 'publish' },
+						},
+						{
+							label: 'Referencias',
+							collapsed: false,
+							autogenerate: { directory: 'reference' },
+						},
+					]
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/al/guides/enviroment.md
@@ -0,0 +1,24 @@
+---
+title: Ambiente de desarrollo
+description: A guide in my new Starlight docs site.
+---
+
+### Visual Studio Code (Editor de Codigo)
+
+[https://code.visualstudio.com/](https://code.visualstudio.com/)
+
+### Extension AL para Microsoft Dynamics 365
+
+[https://marketplace.visualstudio.com/items/?itemName=ms-dynamics-smb.al](https://marketplace.visualstudio.com/items/?itemName=ms-dynamics-smb.al)
+
+### Extension AZ AL Dev Tools/AL Code Outline (Visor de Objetos)
+
+[https://marketplace.visualstudio.com/items/?itemName=andrzejzwierzchowski.al-code-outline](https://marketplace.visualstudio.com/items/?itemName=andrzejzwierzchowski.al-code-outline)
+
+### Git (Manejador de Versiones)
+
+[https://git-scm.com/downloads](https://git-scm.com/downloads)
+
+### GitHub (Repositorio)
+
+[https://github.com/](https://github.com/)
